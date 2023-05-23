857a54461f22af0fbee41ca014a05ef63745ae73
Sushil Behera <sbehera@mpgbis.com>
2023-05-23 20:00:12 +0530

    update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.dynamicssquare.ca',
+    siteUrl: 'http://localhost:3000/',
     generateRobotsTxt: false,
     sitemapSize: 7000,
   }
