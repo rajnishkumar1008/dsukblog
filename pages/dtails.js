d57889715c0ca83ff1e8d7566d2bc65ed675e967
Sushil Behera <sbehera@mpgbis.com>
2023-05-24 18:06:18 +0530

    detailpage add

---
+++ pages/dtails.js
@@ -0,0 +1,234 @@
+import Head from "next/head";
+import Image from "next/image";
+import Script from "next/script";
+import Link from "next/link";
+const Blogslugs = ({ blogs }) => {
+  return (
+    <>
+
+
+              <Head>
+              <title>test</title>
+              <meta name="description" content={item.meta_description} />
+              <meta name="viewport" content={item.meta_keyword} />
+              <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+
+
+
+
+     
+
+
+      <div className="blogs-inner-main">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="blogs-breadcromb">
+                <nav aria-label="breadcrumb">
+                  <ol className="breadcrumb">
+                    <li className="breadcrumb-item"><a href="/">Home</a></li>
+                    <li className="breadcrumb-item"><a href="#">Blog</a></li>
+                    <li className="breadcrumb-item active">Data</li>
+                  </ol>
+                </nav>
+              </div>
+            </div>
+          </div>
+
+          <div className="row g-5">
+            <div className="col-lg-8">
+              <div className="blogs-single-post-sec">
+              <div className="blogs-lates blogs-lates-top-head">
+                <h1>
+                  <a href="/">Power BI vs Looker: Which Suits you Better?</a>
+                </h1>
+                <div className="blogs-info-list">
+                  <div className="left-list">
+                  <span className="user">
+                    <a href="/">
+                      <i class="bi bi-person-circle"></i> Alice Jackson
+                    </a>
+                  </span>
+                  <span className="date">
+                    <a href="/">
+                      <i class="bi bi-calendar"></i> March 1, 2023
+                    </a>
+                  </span>
+                  <span className="time">
+                    <a href="/">
+                      <i class="bi bi-clock"></i> 9mins read
+                    </a>
+                  </span>
+                  <span className="cate">
+                    <a href="/">
+                      <i class="bi bi-app"></i> Business Central
+                    </a>
+                  </span>
+                  </div>
+
+                  <div className="sociallist">
+                  <a target="_blank" rel="" href="https://twitter.com/dsquare_ca" class="twitter" aria-label="visit twitter"><i class="bi bi-twitter"></i></a>
+                  <a target="_blank" rel="" href="https://www.facebook.com/dynamicssquareca" class="facebook" aria-label="visit facebook"><i class="bi bi-facebook"></i></a>
+                  <a target="_blank" rel="" href="https://www.linkedin.com/company/dynamics-square-canada/" class="linkedin" aria-label="visit linkedin"><i class="bi bi-linkedin"></i></a>
+                  </div>
+                </div>
+              </div>
+              <div className="blogs-content">
+                <p>Dynamics 365 on-premises to cloud migration is the hot topic around business owners or decision makers. However, before we get to the answer of whether you should migrate your existing system from on-premises to the cloud or not, let’s take a look at the situation that business owners are currently in.</p>
+
+                <p>You may have heard the quote, “Data is everything”. But do you understand the concept behind it?</p>
+
+                <p>We are living in a world where our data is being tracked, regardless of whether it is personal or professional, and with or without our consent. Yes, it is a bit harsh, but it is the unspoken truth.</p>
+                <h3>Power BI vs Looker: Most Detailed Comparison</h3>
+
+                <p>We are living in a world where our data is being tracked, regardless of whether it is personal or professional, and with or without our consent. Yes, it is a bit harsh, but it is the unspoken truth.</p>
+
+                <p>We are living in a world where our data is being tracked, regardless of whether it is personal or professional, and with or without our consent. Yes, it is a bit harsh, but it is the unspoken truth.</p>
+                <h3>Power BI vs Looker: Most Detailed Comparison</h3>
+
+<p>We are living in a world where our data is being tracked, regardless of whether it is personal or professional, and with or without our consent. Yes, it is a bit harsh, but it is the unspoken truth.</p>
+
+<p>We are living in a world where our data is being tracked, regardless of whether it is personal or professional, and with or without our consent. Yes, it is a bit harsh, but it is the unspoken truth.</p>
+
+
+              <div className="sociallist sociallist-bott">
+                  <a target="_blank" rel="" href="https://twitter.com/dsquare_ca" class="twitter" aria-label="visit twitter"><i class="bi bi-twitter"></i></a>
+                  <a target="_blank" rel="" href="https://www.facebook.com/dynamicssquareca" class="facebook" aria-label="visit facebook"><i class="bi bi-facebook"></i></a>
+                  <a target="_blank" rel="" href="https://www.linkedin.com/company/dynamics-square-canada/" class="linkedin" aria-label="visit linkedin"><i class="bi bi-linkedin"></i></a>
+                  </div>
+              </div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+            <div className="prom-bann">
+                <img src="/img/blog-pic.jpg" alt="bg-pic" />
+              </div>
+            </div>
+          </div>
+
+
+          <div className="row pd-90">
+            <div className="col-lg-6">
+              <div className="blogs-lates blogs-lates-repet">
+                  <h3>
+                    <a href="/">Power BI vs Looker: Which Suits you Better?</a>
+                  </h3>
+                  <div className="blogs-info-list">
+                    <span className="user">
+                      <a href="/">
+                        <i class="bi bi-person-circle"></i> Alice Jackson
+                      </a>
+                    </span>
+                    <span className="date">
+                      <a href="/">
+                        <i class="bi bi-calendar"></i> March 1, 2023
+                      </a>
+                    </span>
+                    <span className="time">
+                      <a href="/">
+                        <i class="bi bi-clock"></i> 9mins read
+                      </a>
+                    </span>
+                    <span className="cate">
+                      <a href="/">
+                        <i class="bi bi-app"></i> Business Central
+                      </a>
+                    </span>
+                  </div>
+                  <div className="b-card-info">
+                    <p>
+                      A complete solution to manage your end-to-end finances,
+                      automate operations, iron out accounting complexities...
+                    </p>
+                    <div className="page-link-read">
+                      <a href="/">
+                        Read More <span>{">"}</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="blogs-lates blogs-lates-repet">
+                  <h3>
+                    <a href="/">Power BI vs Looker: Which Suits you Better?</a>
+                  </h3>
+                  <div className="blogs-info-list">
+                    <span className="user">
+                      <a href="/">
+                        <i class="bi bi-person-circle"></i> Alice Jackson
+                      </a>
+                    </span>
+                    <span className="date">
+                      <a href="/">
+                        <i class="bi bi-calendar"></i> March 1, 2023
+                      </a>
+                    </span>
+                    <span className="time">
+                      <a href="/">
+                        <i class="bi bi-clock"></i> 9mins read
+                      </a>
+                    </span>
+                    <span className="cate">
+                      <a href="/">
+                        <i class="bi bi-app"></i> Business Central
+                      </a>
+                    </span>
+                  </div>
+                  <div className="b-card-info">
+                    <p>
+                      A complete solution to manage your end-to-end finances,
+                      automate operations, iron out accounting complexities...
+                    </p>
+                    <div className="page-link-read">
+                      <a href="/">
+                        Read More <span>{">"}</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="bootom-blogs-sub">
+          <div className="container">
+            <div className="row justify-content-center row-bg">
+            <div className="col-lg-6">
+                <div className="form-subscriber-card">
+                   <div className="sub-head">
+                        <h3>Join our newsletter</h3>
+                        <p>Engaging stories and exclusive data, designed for our best customers. We only send one issue each month, so we try to make it useful.</p>
+                   </div>
+                <form className="sbb-form">
+                <div className="mb-3">
+                    <input
+                    type="email"
+                    className="form-control"
+                    placeholder="* Work Email"
+                    name="email"
+                    required
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary fomr-submit">
+                Subscribe
+                </button>
+                <div className="" role="status">
+                <span className="visually-hidden">Loading...</span>
+                </div>
+                </form>
+                </div>
+            </div>
+            </div>
+          </div>
+        </div>
+      
+    </>
+  );
+};
+
+export default Blogslugs;
